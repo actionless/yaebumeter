25ccfc5e734db0a664b36ef2325b687ed8a4bf3d
actionless <actionless.loveless@gmail.com>
2022-10-12 00:40:58 +0200

    chore(makefile): return and comment out orig build options

--- build_linux_localdir.sh
+++ build_linux_localdir.sh
@@ -5,8 +5,6 @@ mkdir -p "${SCRIPT_DIR}/fake_share/share"
 rm "${SCRIPT_DIR}/fake_share/share/ebumeter"
 ln -s "$SCRIPT_DIR/share" "${SCRIPT_DIR}/fake_share/share/ebumeter"
 
-sed -e '/native/d' \
-  -i source/Makefile
 #make -C source PREFIX=/usr
 #make clean -C source
 make -C source PREFIX="${SCRIPT_DIR}/fake_share/" CXXFLAGS=-g
